8a81c7f6ba1308127703e7d95e02eb4aa8e48b2c
JosieKoelsch <koelschjc@gmail.com>
2022-06-30 16:47:23 +0200

    Created exit button to return from exercises

---
+++ ParameterCurve/Assets/Scripts/Behaviour/Button/ExitButtonBehavior.cs
@@ -0,0 +1,51 @@
+using Controller;
+using Model;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using HTC.UnityPlugin.Vive;
+using UI;
+
+namespace Behaviour.Button
+{
+    /// <summary>
+    /// ***If exercises are converted to a separate scene, this class should 
+    /// not be used and instead use the ReturnToRoomButtonBehavior.cs script***
+    /// </summary>
+    public class ExitButtonBehavior : AbstractButtonBehaviour
+    {
+        /// <summary>
+        /// Single world state controller instance <see cref="WorldStateController"/>
+        /// </summary>
+        public WorldStateController world;
+
+        /// <summary>
+        /// Vive Rig to move to other room upon button press
+        /// </summary>
+        public GameObject VR;
+
+        public CurveSelectionControl curveControl;
+
+        /// <summary>
+        /// Unity Start function
+        /// ====================
+        /// 
+        /// This function is called before the first frame update
+        /// </summary>
+        protected new void Start()
+        {
+            base.Start();
+            gameObject.SetActive(GlobalDataModel.InitFile.ApplicationSettings.TableSettings.ShowNavButtons);
+        }
+
+        #region Clicked
+        protected override void HandleButtonEvent()
+        {
+            //new room is +20 units in x direction from main room
+            VR.transform.position = VR.transform.position + new Vector3(-20, 0, 0);
+            curveControl.SwitchCurveGroup(GlobalDataModel.CurveDisplayGroup.Display);
+        }
+        #endregion Clicked
+    }
+}
